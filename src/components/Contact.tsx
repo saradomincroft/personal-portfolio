9fb5fe9816862e2226ff4cb226098c1182e2e0f2
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2025-05-05 13:27:24 +1000

    more projects styling added pagenation, also minor styling on h2s across project

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,9 @@
 const Contact = () => {
     return (
         <div
-            id="contact-section"
-            className="flex flex-col items-center text-center pt-16 px-4 min-h-screen"
-        >            
+        id="contact-section"
+        className="min-h-screen pt-16 px-4 text-[#F1F1F1]"
+      >
         <h2 className="text-4xl font-bold mb-8 border-b-2">Contact</h2>
         <div>
 
